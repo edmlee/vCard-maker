8454ce85f608bea71a5d072a372b1adef14a3f34
Eddie Lee <112559345+edmlee@users.noreply.github.com>
2022-09-13 20:51:12 +1000

    Fixed NoneType issue due to empty cells

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# vCard-maker  
+# vCard Maker  
 
-Automatically create vCards from a .xlsx file  
+Create multiple vCards simultaneously by reading contacts from an `.xlsx` file  
 Instructions:  
-1. Create a "Contacts" folder in the same directory if it does not already exist
-2. Add contact details into the "contact_list.xlsx" spreadsheet  
-3. Modify "num_of_contacts" variable to the desired number of contacts per vCard  
-4. Run "vcard_maker.py"  
-5. All vCards will be created in the "Contacts" folder and will overwrite all existing files  
+1. Create a `Contacts` folder in the same directory if it does not already exist
+2. Add contact details into the `contact_list.xlsx` spreadsheet provided  
+3. Modify `num_of_contacts` variable to the desired number of contacts per vCard  
+4. Run `vcard_maker.py`  
+5. All vCards will be created in the `Contacts` folder and will overwrite all existing files  
+6. The name of the `.vcf` file is given by the value in the `ID` column
--- vcard_maker.py
+++ vcard_maker.py
@@ -9,9 +9,9 @@ class Contact():
         self.id = id
 
 
-def clear_vcard(device):
+def clear_vcard(id):
     try:
-        file = open(f"./Contacts/Contacts {device}.vcf", "w")
+        file = open(f"./Contacts/Contacts {id}.vcf", "w")
         file.close()
     except FileNotFoundError:
         print(f"Missing \"{folder_name}\" folder in current directory")
@@ -31,26 +31,38 @@ def print_vcard(contact):
 wb = load_workbook("contact_list.xlsx")
 ws = wb["Contacts"]
 folder_name = "Contacts" # Folder must be created in the main directory. Used to store vCards
-device_col = 1
+id_col = 1 # This column will provide the name for the .vcf file
 first_name_col = 2
 last_name_col = 3
 phone_col = 4
 
-num_of_contacts = 14 # Number of contacts to add per vCard
-num_of_devices = int((ws.max_row - 1) // num_of_contacts)
+num_of_contacts = 14 # Number of contacts to add per vCard. Modify as required
+num_of_ids = int((ws.max_row - 1) // num_of_contacts)
 row = 2
 
-for i in range(num_of_devices):
-    device = int(ws.cell(row, device_col).value)
-    clear_vcard(device)
+for i in range(num_of_ids):
+    id = int(ws.cell(row, id_col).value)
+    clear_vcard(id)
 
     for j in range(row, row + num_of_contacts):
         try:
-            with open(f"./{folder_name}/Contacts {device}.vcf", "a") as file:
+            with open(f"./{folder_name}/Contacts {id}.vcf", "a") as file:
                 first_name = ws.cell(j, first_name_col).value
                 last_name = ws.cell(j, last_name_col).value
-                phone = int(ws.cell(j, phone_col).value)
-                contact = Contact(first_name, last_name, phone, device)
+
+                # Convert empty cells into blank text
+                if first_name is None:
+                    first_name = ""
+                if last_name is None:
+                    last_name = ""
+                
+                # Convert invalid phone number into blank text
+                try:
+                    phone = int(ws.cell(j, phone_col).value)
+                except TypeError:
+                    phone = ""
+
+                contact = Contact(first_name, last_name, phone, id)
                 print_vcard(contact)
         except FileNotFoundError:
             print(f"Missing \"{folder_name}\" folder in current directory")
